18f92ec025dcad2287e36ad2b3f36d03dc6390b0
srijan0104 <76627584+srijan0104@users.noreply.github.com>
2021-06-17 10:36:19 +0530

    Add files via upload

---
+++ typosetup.py
@@ -0,0 +1,42 @@
+import sys
+from cx_Freeze import *
+
+
+includefiles = ['Letter-T-icon_34797.ico']
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+shortcut_table = [
+    ("DesktopShortcut",  # Shortcut
+     "DesktopFolder",  # Directory_
+     "Typospeed",  # Name
+     "TARGETDIR",  # Component_
+     "[TARGETDIR]\main.exe",  # Target
+     None,  # Arguments
+     None,  # Description
+     None,  # Hotkey
+     None,  # Icon
+     None,  # IconIndex
+     None,  # ShowCmd
+     "TARGETDIR",  # WkDir
+     )
+]
+msi_data = {"Shortcut": shortcut_table}
+
+# Change some default MSI options and specify the use of the above defined tables
+bdist_msi_options = {'data': msi_data}
+setup(
+    version="1.0",
+    description="Typing speed game",
+    author="ITW",
+    name="Typospeed",
+    options={'build_exe': {'include_files': includefiles}, "bdist_msi": bdist_msi_options, },
+    executables=[
+        Executable(
+            script="main.py",
+            base=base,
+            icon='Letter-T-icon_34797.ico',
+        )
+    ]
+)
