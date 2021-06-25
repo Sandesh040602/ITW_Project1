c386555fe9ff8eecbe1b48da0cdc6df1b75d3d3d
srijan0104 <76627584+srijan0104@users.noreply.github.com>
2021-06-25 12:54:20 +0530

    Create main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-### all imports used
+#all the necessary imports required in the project.
 from tkinter import *
-from PIL import Image,ImageTk
+from PIL import Image,ImageTk#basically for background image.
 import random
 from tkinter import messagebox
 
@@ -9,6 +9,11 @@ words = ['test', 'rescue', 'interfere', 'light', 'woozy', 'fetch', 'wrong', 'bru
 
 #defined functions
 def labelslider():
+    '''
+    This is the function responsible for sliding the the text  : '~~~WELCOME TO TYPOSPEED~~~~' . under this first we pass an empty string , and a count variable starting with 0 
+    uptil the length of passed text . Now the empty string (sliderwords) keeps on getting updated with each next character from the passed string , and this we have fixed after 
+    120 microseconds , the function labelslider will be called under the fontlabel.
+    '''
     global count, sliderwords
     text = '~~~WELCOME TO TYPOSPEED~~~~'
 
@@ -22,6 +27,11 @@ def labelslider():
 
 
 def time():
+    '''
+    This function is responsible for the timer of 60 seconds we have put , under the timeleft lable . We will be basically decrementing the 60 to 0 , with color change from
+    green to red as it slips below 10 , as a mark of DANGER / HURRY . To make this decrementation in intervals of seconds , we call the function time under timerlabelcount 
+    after 1000 ms using the after() function . 
+    '''
     global timeleft, score, miss
     if (timeleft >= 11):
         pass
@@ -32,45 +42,50 @@ def time():
         timerlabelcount.configure(text=timeleft)
         timerlabelcount.after(1000, time)
     else:
+        #When the timeleft becomes less than 0 . The lable consisting of the final score flashes on the screen . 
         hintlabel.configure(text='Hit = {} | Miss = {} | Total Score = {}'.format(score, miss, score - miss))
+        #Along with it comes a message box , asking for a retry or cancel .
         rr = messagebox.askretrycancel('Notification', 'To try again click on Retry')
         if (rr == True):
-
+            #If a person clicks on retry , then everything is reseted using following block of code.
             score = 0
             timeleft = 60
             miss = 0
-            timerlabelcount.configure(text=time, fg='green')
+            timerlabelcount.configure(text=time, fg='green')#Timer font color reseted back to green .
             timerlabelcount.configure(text=timeleft)
-            wordlabel.configure(text=words[0])
+            wordlabel.configure(text=words[0])#The display of words also resetted.
             scorelabelcount.configure(text=score)
 
 
 def startgame(event):
+    '''
+    This function is responsible for the running of whole game . 
+    '''
     global score, miss
-    if (timeleft == 60):
+    if (timeleft == 60): #if timeleft is at 60 , time() function is called , which runs the timer.
         time()
-    hintlabel.configure(text='')
+    hintlabel.configure(text='')#with the starting of timer , the hintlabel's text is made an empty string so that it is out of screen.
 
-    if (wordentry.get() == wordlabel['text']):
+    if (wordentry.get() == wordlabel['text']):#incrementing the score if the input string matches with any of it in the wordlist.
         score += 1
         scorelabelcount.configure(text=score)
 
     else:
-        miss += 1
+        miss += 1#if input string is unmatched then miss is incremented .
 
-    random.shuffle(words)
+    random.shuffle(words)#using the shuffle function from the random variables , we are calling random strings from the wordlist. 
     wordlabel.configure(text=words[0])
 
-    wordentry.delete(0, END)
+    wordentry.delete(0, END)#this line clears the entry widget after <return> is pressed from the keyboard.
 
 
 
 #main window of GUI
-root = Tk()
-root.wm_geometry('800x900')
+root = Tk()#forms the main GUI window
+root.wm_geometry('800x900')#defined the geometry of the main window.
 
 
-
+#applying background image for the main GUI window.
 load = Image.open(r'r.jpg')
 render = ImageTk.PhotoImage(load)
 img = Label(root, image=render)
@@ -95,7 +110,7 @@ miss = 0
 
 ####labels
 
-img2 = PhotoImage(file = r'new.png')
+img2 = PhotoImage(file = r'C:\Users\sanka\Desktop\new.png')
 l1 = Label(root,image = img2,bg='SkyBlue1')
 l1.place(x=40,y=40)
 
